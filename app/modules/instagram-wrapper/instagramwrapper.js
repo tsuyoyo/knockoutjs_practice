637bb22573cfa79b5ca9a5f1ce88becf0c9ea2a5
tsuyoyo <tsuyogoro@gmail.com>
2013-12-08 22:35:48 +0900

    Solved same origin policy issue of instagram API

--- app/modules/instagram-wrapper/instagramwrapper.js
+++ app/modules/instagram-wrapper/instagramwrapper.js
@@ -3,25 +3,42 @@ define(function() {
   var CLIENT_ID = 'd381d0763e79446b86ddfc88988f5c7a';
 
   function searchImageByTag(tag, callback) {
-    var url = 'https://api.instagram.com/v1/tags/' + tag + '/media/recent?client_id=' + CLIENT_ID;
-    callApi(url, callback, parseJson);
+    var url = 'https://api.instagram.com/v1/'
+              + 'tags/' + tag 
+              + '/media/recent?client_id=' + CLIENT_ID;
+    callJsonp(url, callback, parseJson);
   }
 
   function parseJson(jsonData) {
-    return [];
+    var data = jsonData.data;
+    var images = [];
+    for (var i=0; i<data.length; i++) {
+      images.push(createImageData(data[i]));
+    }
+    return images;
   }
 
+  function createImageData(dataEntry) {
+    var contentUrl = dataEntry.images.standard_resolution.url;
+    var thumbnail  = dataEntry.images.low_resolution.url;
+    var titleCaption = dataEntry.caption.text;
+    return {contentUrl: contentUrl, thumbnail: thumbnail, title: titleCaption};
+  }
 
-  function callApi(apiUrl, callback, jsonParser) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', apiUrl, true);
-    xhr.onreadystatechange = function(){
-      if (xhr.readyState === 4 && xhr.status === 200){
-        var jsonData = JSON.parse(xhr.responseText);
+  function callJsonp(url, callback, jsonParser) {
+    // Set callback.
+    // Referred : http://requirejs.org/docs/api.html#jsonp
+    var jsonpUrl = url + '&callback=define';
+    require([jsonpUrl],
+      function(jsonData) {
         callback(jsonParser(jsonData));
       }
-    }
-    xhr.send();
+    );
+    // Call JSONP api.
+    var target = document.createElement('script');
+    target.charset = 'utf-8';
+    target.src = url;
+    document.body.appendChild(target);
   }
 
   return {
